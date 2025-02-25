fff6117a56b4b26a47b757e10bcf4e130b56f1a2
Caiza Tacuri Alisson Lisbeth <84515234+alissoncaiza@users.noreply.github.com>
2025-02-25 08:39:01 -0500

    Servidor de Autenticación OAuth

---
+++ AuthServer/src/main/java/com/espe/edu/AuthServer/dto/RegistroRequest.java
@@ -0,0 +1,40 @@
+package com.espe.edu.AuthServer.dto;
+
+public class RegistroRequest {
+    private String nombre;
+    private String email;
+    private String password;
+    private String rol; // Puede ser "ADMINISTRADOR" o "CLIENTE"
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+}
